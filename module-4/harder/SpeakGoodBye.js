745961282c53fd3c35732bc07ae47f99bcfcab85
Laura <laura.stievenard@capgemini.com>
2022-06-02 16:16:47 +0200

    Update index.html dans SpeakGoodBye.js

--- module-4/harder/SpeakGoodBye.js
+++ module-4/harder/SpeakGoodBye.js
@@ -1,7 +1,7 @@
 (function (window) {
   var byeSpeaker = {};
   var speakWord  = "Good Bye";
-  function speak(name) {
+  byeSpeaker.speak = function (name) {
     console.log(speakWord + " " + name);
   }
   window.byeSpeaker = byeSpeaker;
